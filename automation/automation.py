74642b7bb75b6c66bc17187fd937da644c363285
MasonChance <mason.fryberger@gmail.com>
2021-02-10 20:37:12 -0800

    tests written and script file written

---
+++ automation/automation.py
@@ -0,0 +1,34 @@
+import re
+
+
+# define funciton/match variables with regex to be run on the files in question
+
+def phone_re(line):
+  pass
+
+
+def email_re(line):
+  pass
+
+
+# define function to format phone numbers (add 206 area code if area code missing)
+def phone_form(phone):
+  pass
+
+
+# define a function that checks for duplication to be called within the write methods prior the finish of the write method
+def check_dupe(potential):
+  pass
+
+
+# define function to write the phone numbers and emails to their respective files.
+
+def write_email(email):
+  pass
+
+def write_phone(phone):
+  pass
+
+# read input file, call the write fn for email and phone. 
+
+# define main 
