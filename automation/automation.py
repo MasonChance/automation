fa35ff29ec9ff8e14bb4c5fd36963f577071dc45
MasonChance <mason.fryberger@gmail.com>
2021-02-12 15:55:12 -0800

    phone and email regex working, tests passing,

--- automation/automation.py
+++ automation/automation.py
@@ -3,28 +3,48 @@ import re
 
 # define funciton/match variables with regex to be run on the files in question
 
-def phone_re(line):
-  regi = re.compile(r'\(\\d{3}\)[-.]\\d{3}[-.]\\d{4}(x\\d{5})?')
+def phone_re(line:str)->list:
+  regi = re.compile(r'(\(\d{3}\)|(\d{3}[-.])?\d{3}[-.]\d{4}(x\d{1,5})?)')
   matches = re.findall(regi, line)
-  print(matches)
+  match_list = []
+
   for num in matches:
-    print(f'num in list of matches: {str(num)}')
+    match_list.append(str(num[0]))
 
-  return matches
+  return match_list
+
   
-
-def email_re(line):
-  pass
-
-
+def email_re(line:str)->list:
+  regi = r'(\w*@((\w*)|(\w*[-_]\w*))?[.]\w{3})'
+  matches = re.findall(regi, line)
+  match_list = []
+  
+  for mail in matches:
+    match_list.append(str(mail[0]))
+  
+  return match_list
+  
 # define function to format phone numbers (add 206 area code if area code missing)
-def phone_form(phone):
-  pass
+def phone_form(phone:str)->str:
+  if len(phone) < 8:
+    phone = '\n'
+  if len(phone) == 8:
+    phone = '206-'+ phone
+  if '(' in phone:
+    phone = phone[1:4] + '-' + phone[6:len(phone)]
+  if 'x' in phone:
+    phone = phone[0:phone.index('x')]
+
+  return phone 
+
 
 
 # define a function that checks for duplication to be called within the write methods prior the finish of the write method
-def check_dupe(potential):
-  pass
+def check_dupe(potential:str, saved:list)->bool:
+  if potential in saved:
+    return True
+  return False
+  
 
 
 # define function to write the phone numbers and emails to their respective files.
@@ -40,3 +60,4 @@ def write_phone(phone):
 # define main 
 if __name__ == "__main__":
   phone_re('861-5898 1-178-383-0937 001-048-736-2919')
+  email_re(' eye. danielletaylor@hotmail.com Less  character. hsoto@sharp-king.org See')
--- tests/test_automation.py
+++ tests/test_automation.py
@@ -11,47 +11,68 @@ def test_version():
 
 # @pytest.mark.skip('pending code')
 def test_phone_regex(strings_with_phone_number):
-    phone = ''
     match = phone_re(strings_with_phone_number)
-    actual = phone
-    expected = '.861-5898 lif 1-178-383-0937Mat bog001-048-736-2919'
+    actual = match
+    expected = ['861-5898', '178-383-0937x54779', '048-736-2919']
     assert actual == expected
 
-@pytest.mark.skip('pending code')
+# @pytest.mark.skip('pending code')
 def test_email_regex(strings_with_email):
-    email = []
-    for line in strings_with_email:
-        match = email_re(line)
-        email.append(match)
-    
-    actual = email
+    match = email_re(strings_with_email)
+    actual = match
     expected = ['danielletaylor@hotmail.com', 'hsoto@sharp-king.org']
     assert actual == expected
 
 
-@pytest.mark.skip('pending code')
+# @pytest.mark.skip('pending code')
 def test_phone_formatter_with_ext(strings_with_phone_number):
-    w_ext = strings_with_phone_number[1]
-    actual = phone_form(w_ext)
-    expected = '1-178-383-0937'
+    w_ext = phone_re(strings_with_phone_number)
+    actual = phone_form(w_ext[1])
+    expected = '178-383-0937'
     assert actual == expected
 
 
-@pytest.mark.skip('pending code')
+# @pytest.mark.skip('pending code')
 def test_phone_formatter_with_missing_area(strings_with_phone_number):
-    w_missing = strings_with_phone_number[0]
-    actual = phone_form(w_missing)
+    w_missing = phone_re(strings_with_phone_number)
+    actual = phone_form(w_missing[0])
     expected = '206-861-5898'
     assert actual == expected
 
 
-@pytest.mark.skip('pending code')
+# @pytest.mark.skip('pending code')
 def test_phone_formatter_with_13_digits(strings_with_phone_number):
-    w_13 = strings_with_phone_number[2]
-    actual = phone_form(w_13)
+    w_13 = phone_re(strings_with_phone_number)
+    actual = phone_form(w_13[2])
     expected = '048-736-2919'
     assert actual == expected
 
+def test_check_dupe_with_phone_no_dupe(strings_with_phone_number):
+    dupe = phone_re(strings_with_phone_number)
+    actual = check_dupe('206-412-6226', dupe)
+    expected = False
+    assert actual == expected
+
+
+def test_check_dupe_with_phone_yes_dupe(strings_with_phone_number):
+    dupe = phone_re(strings_with_phone_number)
+    actual = check_dupe(dupe[0], dupe)
+    expected = True
+    assert actual == expected
+
+
+def test_check_dupe_with_email_no_dupe(strings_with_email):
+    dupe = email_re(strings_with_email)
+    actual = check_dupe('frogger@gmail.com', dupe)
+    expected = False
+    assert actual == expected
+
+def test_check_dupe_with_email_yes_dupe(strings_with_email):
+    dupe = email_re(strings_with_email)
+    actual = check_dupe(dupe[0], dupe)
+    expected = True
+    assert actual == expected
+    
 
 @pytest.mark.skip('pending code')
 def test_phone_writer():
@@ -78,10 +99,10 @@ def test_email_writer():
 
 @pytest.fixture
 def strings_with_phone_number():
-  return ['find.861-5898Especially com +1-178-383-0937x54779Th 001-048-736-2919Much']
+  return 'find.861-5898Especially com +1-178-383-0937x54779Th 001-048-736-2919Much'
 
 @pytest.fixture  
 def strings_with_email():
-    return [' eye. danielletaylor@hotmail.com Less  character. hsoto@sharp-king.org See']
+    return ' eye. danielletaylor@hotmail.com Less  character. hsoto@sharp-king.org See'
 
 
